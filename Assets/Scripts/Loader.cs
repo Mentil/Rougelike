4581a31420f9515e6747c34fbc9665f5870e9861
mentil <doc0oc@gmail.com>
2020-01-18 23:25:18 +0300

    quit game on escape

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -19,5 +19,13 @@ namespace Assets.Scripts
             if (SoundManager.instance == null)
                 Instantiate(soundManager);
         }
+
+        void Update()
+        {
+            if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
+        }
     }
 }
